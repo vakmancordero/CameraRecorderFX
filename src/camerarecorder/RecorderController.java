ebbbe4b119c81ec3344d71800605bf3c39f3ef88
vakmancordero <vakmancordero@gmail.com>
2017-06-23 12:25:18 -0500

    Añadiendo proyecto

---
+++ src/camerarecorder/RecorderController.java
@@ -0,0 +1,18 @@
+package camerarecorder;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class RecorderController implements Initializable {
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+    
+    
+    
+}
